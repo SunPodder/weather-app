75c808d7b18c097042b7238fe1e9df8542d70700
Sun <73415680+SunPodder@users.noreply.github.com>
2021-08-26 22:24:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const api_key = "3f0d9e3372aab53a19fc6f5527ec10cf",
+const api_key = "Get Your API Key from https://openweather.org/api",
   input = document.querySelector("#input"),
   form = document.querySelector("form"),
   container = document.querySelector("#container"),
@@ -8,6 +8,7 @@ const api_key = "3f0d9e3372aab53a19fc6f5527ec10cf",
   container4 = document.querySelector("#container4"),
   container5 = document.querySelector("#container5")
 
+//Get data based on city name given by User
 form.addEventListener("submit", e => {
   e.preventDefault()
   let cityName = input.value
@@ -15,6 +16,7 @@ form.addEventListener("submit", e => {
   getData(url)
 })
 
+//Function to fetch data from URL
 function getData(url){
   fetch(url)
   .then(res => {
@@ -29,7 +31,9 @@ function getData(url){
 function writeData(list){
   list.forEach(item => {
     let li = document.createElement("li")
+    //Converting temperature from Kelvin to Celsius
     let temp = Math.round(+(item.main.temp) - 273) + "°C"
+      //minmum and maximum temperature incase you need them.
       /*min_temp = item.main.temp_min,
       max_temp = item.main.temp_max,*/
     let pressure = item.main.pressure,
@@ -41,6 +45,7 @@ function writeData(list){
     let dateTime = item.dt_txt,
       iconCode = weather.icon,
       main = weather.main,
+      //svg weather icon
       icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${iconCode}.svg`;
     li.innerHTML = `
     <div class="card">
@@ -76,17 +81,21 @@ function writeData(list){
 function getLocation(){
   if(navigator.geolocation){
     navigator.geolocation.getCurrentPosition(useLocation, onErr, constraints)
+  }else{
+    alert("Geo Location isn't supported by your browser. Please enter your city name in the input field")
   }
 }
 const constraints = {
   enableHighAccuracy: true
 }
+//Get weather data based on GPS coordinates
 function useLocation(pos){
   let lat = pos.coords.latitude
   let lon = pos.coords.longitude
   let url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`
   getData(url)
 }
+//incase of geo location error
 function onErr(err){
   alert("Please allow location access.\nError: " + err.message)
 }
